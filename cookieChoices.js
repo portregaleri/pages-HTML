ee297a7bc289c303299c5d7fe2a6cefe5e0be67a
portregaleri <32998767+portregaleri@users.noreply.github.com>
2017-10-22 13:52:22 +0300

    Add files via upload

---
+++ cookieChoices.js
@@ -0,0 +1,9 @@
+﻿//<![CDATA[
+  document.addEventListener('DOMContentLoaded', function(event) {
+    window.cookieChoices && cookieChoices.showCookieConsentBar && cookieChoices.showCookieConsentBar(
+        (window.cookieOptions && cookieOptions.msg) || 'İçeriği ve reklamları kişiselleştirmek, sosyal medya özellikleri sunmak ve trafiği analiz etmek için çerezler kullanıyoruz.',
+        (window.cookieOptions && cookieOptions.close) || 'Kabul Ediyorum',
+        (window.cookieOptions && cookieOptions.learn) || 'Ayrıntıları inceleyin',
+        (window.cookieOptions && cookieOptions.link) || 'https://www.blogger.com/go/blogspot-cookies');
+  });
+//]]>
