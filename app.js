773c8d615e36ddc77a9840fcb5b8cb081147e744
portregaleri <32998767+portregaleri@users.noreply.github.com>
2017-11-06 00:29:36 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+.fail(function(data) {
+    // Make sure that the formMessages div has the 'error' class.
+    $(formMessages).removeClass('success');
+    $(formMessages).addClass('error');
+
+    // Set the message text.
+    if (data.responseText !== '') {
+        $(formMessages).text(data.responseText);
+    } else {
+        $(formMessages).text('Oops! An error occured and your message could not be sent.');
+    }
+});
