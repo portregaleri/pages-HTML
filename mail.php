a25ecf1092287c8ff3f7cbb700e2cf302e0460c6
portregaleri <32998767+portregaleri@users.noreply.github.com>
2017-11-07 02:38:20 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,19 +1,24 @@
-<?php
-	if(isset($_POST['ad']) && isset($_POST['eposta']) && isset($_POST['konu']) && isset($_POST['mesaj'])) {
-	   if(empty($_POST['ad']) || empty($_POST['eposta']) || empty($_POST['headers'])|| empty($_POST['konu']) || empty($_POST['mesaj'])) {
-	      echo 'Lütfen boş yer bırakmayın!';
-	   } else {
-		  $headers .= 'To: <portregaleri@gmail.com>' . "\r\n";
-	      $ad = strip_tags($_POST['ad']);
-	      $eposta = strip_tags($_POST['eposta']);
-		  $eposta = 'portregaleri@gmail.com';
-	      $konu = strip_tags($_POST['konu']);
-	      $mesaj = strip_tags($_POST['mesaj']);
-	      $icerik = 'Ad: ' . $ad . '<br/>E-Posta: '. $eposta . '<br/>' . $mesaj;
-	      mail('portregaleri@gmail.com', $konu, $mesaj);
-	      echo 'Mesajınız Gönderildi! Teşekkürler.';
-	   }
-	} else {
-	   echo 'Lütfen Formu Kullanın!';
-	}
+<?
+require("class.mail.php");
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->SMTPDebug = 1; // hata ayiklama: 1 = hata ve mesaj, 2 = sadece mesaj
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'ssl'; // Güvenli baglanti icin ssl normal baglanti icin tls
+$mail->Host = "mail.gmail.com"; // Mail sunucusuna ismi
+$mail->Port = 26; // Gucenli baglanti icin 465 Normal baglanti icin 587
+$mail->IsHTML(true);
+$mail->SetLanguage("tr", "mail/language");
+$mail->CharSet ="utf-8";
+$mail->Username = "portregaleri@gmail.com"; // Mail adresimizin kullanicı adi
+$mail->Password = "midyatli3"; // Mail adresimizin sifresi
+$mail->SetFrom("portregaleri", "Isim"); // Mail attigimizda gorulecek ismimiz
+$mail->AddAddress("portregaleri@gmail.com"); // Maili gonderecegimiz kisi yani alici
+$mail->Subject = "Mesaj Basligi"; // Konu basligi
+$mail->Body = "Mesaj icerigi"; // Mailin icerigi
+if(!$mail->Send()){
+echo "Mailer Error: ".$mail->ErrorInfo;
+} else {
+echo "Mesaj gonderildi";
+}
 ?>
